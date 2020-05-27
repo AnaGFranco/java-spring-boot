a51f82c7b98d61e62bb2cbbe8b18de2d5dad5522
AnaGFranco <anagsfranco@hotmail.com>
2020-05-27 14:45:04 -0300

    Adicionando controle e mapeamento de endpoint

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,5 +1,19 @@
 package br.com.devdojo.awesome.endpoint;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Curso DevDojo - SpringBoot
+ * 
+ * @author Ana Gabriela Franco
+ *
+ */
+
+@RestController  
+@RequestMapping("student")
+
 public class StudentEndpoint {
 
+	
 }
