02517584ccb9f2e49c89858ec94dad6d3f29ae23
AnaGFranco <anagsfranco@hotmail.com>
2020-05-27 17:41:22 -0300

    Metodo formatLocalDateTimeToDateBaseStyle

--- src/main/java/br/com/devdojo/awesome/util/DateUtil.java
+++ src/main/java/br/com/devdojo/awesome/util/DateUtil.java
@@ -1,6 +1,14 @@
 package br.com.devdojo.awesome.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtil {
 	
+	public String formatLocalDateTimeToDateBaseStyle(LocalDateTime localDateTime) {
+		
+		return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm;ss").format(localDateTime);
+		
+	}
 
 }
