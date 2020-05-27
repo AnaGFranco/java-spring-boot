7a73e1ebc5d824df56e31b7026940662a132098a
AnaGFranco <anagsfranco@hotmail.com>
2020-05-27 17:55:39 -0300

    implementa metodo formatLocalDateTimeToDateBaseStyle

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,7 +1,9 @@
 package br.com.devdojo.awesome.endpoint;
 
 import br.com.devdojo.awesome.model.Student;
+import br.com.devdojo.awesome.util.DateUtil;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import static java.util.Arrays.asList;
+
+import java.time.LocalDateTime;
 
 
 /**
@@ -24,8 +28,11 @@ import static java.util.Arrays.asList;
 
 public class StudentEndpoint {
 	
+	@Autowired
+	private DateUtil dateUtil;
 	@RequestMapping(method = RequestMethod.GET,path = "/list")
 	public List<Student> listAll(){
+		System.out.println(dateUtil.formatLocalDateTimeToDateBaseStyle(LocalDateTime.now()));
 		return asList(new Student("Ana"), new Student("Gabriela"));
 	}
 
