af819df69acd6c15768f6dca2399e9ed10582595
AnaGFranco <anagsfranco@hotmail.com>
2020-05-27 17:57:58 -0300

    add component em DateUtil

--- src/main/java/br/com/devdojo/awesome/util/DateUtil.java
+++ src/main/java/br/com/devdojo/awesome/util/DateUtil.java
@@ -3,6 +3,9 @@ package br.com.devdojo.awesome.util;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DateUtil {
 	
 	public String formatLocalDateTimeToDateBaseStyle(LocalDateTime localDateTime) {
